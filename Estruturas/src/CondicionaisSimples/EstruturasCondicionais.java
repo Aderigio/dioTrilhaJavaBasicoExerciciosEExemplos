d03578ab0e035bb62c93b196c762f1c8671d5726
Aderigio <aderigio@hotmail.com>
2023-10-10 13:00:01 -0300

    Estruturas de Repetição - Simples

---
+++ Estruturas/src/CondicionaisSimples/EstruturasCondicionais.java
@@ -0,0 +1,22 @@
+package CondicionaisSimples;
+
+public class EstruturasCondicionais {
+
+	public static void main(String[] args) {
+		//Caixa eletrônico 
+		
+
+		       double saldo = 25.0;
+		       double valorSolicitado = 17.0;
+
+		       if(valorSolicitado < saldo)
+		        saldo = saldo - valorSolicitado;
+
+		        System.out.println(saldo);
+
+		    }
+		
+
+	}
+
+
