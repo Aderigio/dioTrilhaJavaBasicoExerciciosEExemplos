100e109b5e0edce1ca952619b1f37d0bf4baae59
Aderigio <aderigio@hotmail.com>
2023-10-10 11:46:41 -0300

    Incersão de alguns comentários nas entradas de Scanner

--- AboutMe/src/Entidades/AboutMe.java
+++ AboutMe/src/Entidades/AboutMe.java
@@ -4,30 +4,32 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
-    public static void main(String[] args) {
-        //criando o objeto scanner
-    	
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
-        
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
+	public static void main(String[] args) {
+		// criando o objeto scanner
 
-        System.out.println("Digite sua idade");
-        int idade = scanner.nextInt();
-        
-        System.out.println("Digite sua altura");
-        double altura = scanner.nextDouble();
+		// Novo Scanner e uso do Locale para padrão de ponto em vez de virgula
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        
-        //imprimindo os dados obtidos pelo usuario
-        
-        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + "cm ");
-        
-        
-    }
+		// Entrada de uma String pelo Scanner
+		System.out.println("Digite seu nome");
+		String nome = scanner.next();
+
+		System.out.println("Digite seu sobrenome");
+		String sobrenome = scanner.next();
+
+		// Entrada de uma int pelo Scanner
+		System.out.println("Digite sua idade");
+		int idade = scanner.nextInt();
+
+		// Entrada de um double pelo Scanner
+		System.out.println("Digite sua altura");
+		double altura = scanner.nextDouble();
+
+		// imprimindo os dados obtidos pelo usuario
+
+		System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+		System.out.println("Tenho " + idade + " anos ");
+		System.out.println("Minha altura é " + altura + "cm ");
+
+	}
 }
