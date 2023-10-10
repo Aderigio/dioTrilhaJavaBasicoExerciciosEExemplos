a74e4ec4ed7d03fc0b97fb8b6624427a9bba067c
Aderigio <aderigio@hotmail.com>
2023-10-10 13:07:53 -0300

    Condicionais encadeadas adicionada.

---
+++ Estruturas/src/CondicionaisEncadeadas/CondicionaisEncadeadas.java
@@ -0,0 +1,24 @@
+package CondicionaisEncadeadas;
+
+public class CondicionaisEncadeadas {
+
+
+		
+		// ResultadoEscolar.java
+	
+		    public static void main(String[] args) {
+		        int nota = 6;
+
+			if (nota >= 7)
+				System.out.println("Aprovado");
+			else if (nota >= 5 && nota < 7)
+				System.out.println("Recuperação");
+			else
+				System.out.println("Reprovado");
+		   
+		
+		
+
+	}
+
+}
