683fbc7d08725b0dfde689f5e783f49064c22b34
Aderigio <aderigio@hotmail.com>
2023-10-10 13:04:51 -0300

    Condicionais Composta adicionada.

---
+++ Estruturas/src/CondicionaisComposta/CondicionaisComposta.java
@@ -0,0 +1,21 @@
+package CondicionaisComposta;
+
+public class CondicionaisComposta {
+
+	public static void main(String[] args) {
+		
+		
+		// ResultadoEscolar.java
+
+		       int nota = 6;
+		       
+		       if(nota >= 7)
+		        System.out.println("Aprovado");
+
+		       else
+		        System.out.println("Reprovado");
+		
+
+	}
+
+}
